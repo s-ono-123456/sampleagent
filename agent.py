129c11217d9371e6b6b89c84c31e7010a20fdcb8
s-ono <you@example.com>
2025-04-27 00:53:05 +0900

    CustomStateクラスを追加し、ユーザー情報を取得するget_user_info関数を実装。エージェントの作成と呼び出しを更新。

--- agent.py
+++ agent.py
@@ -23,6 +23,10 @@ from typing import Any
 from langchain_core.runnables import ConfigurableField
 from langgraph.graph import END
 from langchain_core.messages import HumanMessage, SystemMessage, AnyMessage, ToolMessage, AIMessage
+from langgraph.prebuilt.chat_agent_executor import AgentState
+from langgraph.prebuilt import create_react_agent
+from langgraph.prebuilt import InjectedState
+
 
 # 環境変数の設定
 # os.environ["LANGSMITH_TRACING"]="true"
@@ -34,3 +38,31 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 MODEL_NAME = "gpt-4.1-nano"
 embedding_model_name = "pkshatech/GLuCoSE-base-ja-v2"
+
+class CustomState(AgentState):
+    user_name: str
+
+def prompt(
+    state: CustomState
+) -> list[AnyMessage]:
+    user_name = state["user_name"]
+    system_msg = f"You are a helpful assistant. User's name is {user_name}"
+    return [{"role": "system", "content": system_msg}] + state["messages"]
+
+def get_user_info(
+    state: Annotated[CustomState, InjectedState]
+) -> str:
+    """Look up user info."""
+    user_id = state["user_id"]
+    return "User is John Smith" if user_id == "user_123" else "Unknown user"
+
+agent = create_react_agent(
+    model=MODEL_NAME,
+    tools=[get_user_info],
+    state_schema=CustomState
+)
+
+agent.invoke({
+    "messages": "hi!",
+    "user_name": "user_123"
+})
