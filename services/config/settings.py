f69ac1a4c8c5cc761413e13225dd9e7d00e9e5c4
s-ono <you@example.com>
2025-05-04 22:51:23 +0900

    fix: LANGSMITH_PROJECTの値を"SimpleAgent"に修正

--- services/config/settings.py
+++ services/config/settings.py
@@ -9,6 +9,6 @@ def setup_environment():
     """環境変数を設定する関数"""
     os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
     os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
-    os.environ["LANGSMITH_PROJECT"] = "Playwright Test"
+    os.environ["LANGSMITH_PROJECT"] = "SimpleAgent"
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
